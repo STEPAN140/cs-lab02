35e2510f7683f031a6c2c6cffb6c1e26ccb84aae
Stepan <stepan.zamitski140@gmail.com>
2024-07-10 11:35:23 +0300

    Min a and b

--- main.cpp
+++ main.cpp
@@ -12,5 +12,6 @@ int main()
     cout << "A * B = " << a * b << '\n';
     cout << "A / B = " << a / b << '\n';
     cout << "Max(A,B) = " << max(a, b) << '\n';
+    cout << "Min(A,B) = " << min(a, b) << '\n';
 }
 
